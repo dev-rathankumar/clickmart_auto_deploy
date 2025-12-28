b85e2d3fa86dd4b84cb448beaa1e615e7ba1349d
Rathan Kumar <developer.rathan@gmail.com>
2025-12-28 23:14:53 +0530

    fixed csrf trusted origins

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -179,4 +179,4 @@ CORS_ALLOWED_ORIGINS = [
     'https://djangoclickmart.store',
 ]
 
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", [])
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
