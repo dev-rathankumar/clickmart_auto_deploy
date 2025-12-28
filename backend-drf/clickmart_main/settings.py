fda5dd70c779c6dd7a35b7c98b2b1708261a3c58
Rathan Kumar <developer.rathan@gmail.com>
2025-12-28 21:42:11 +0530

    minor issue fixed

--- backend-drf/clickmart_main/settings.py
+++ backend-drf/clickmart_main/settings.py
@@ -174,7 +174,7 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-    'http://172.105.33.13:5173'
+    'http://172.105.33.13:5173',
     'http://djangoclickmart.store',
     'https://djangoclickmart.store',
 ]
